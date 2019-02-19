316a8bdafd7d76cc846e143162218cd487b88fcb
Howard Kier <howard@Howards-MacBook-Air.local>
2019-02-18 19:07:53 -0600

    Finished?

--- src/Components/Donate/Donate.js
+++ src/Components/Donate/Donate.js
@@ -13,8 +13,13 @@ const Donate = () => {
                 <p>
                     Monetary contributions to my campaign are very helpful and greatly appreciated.
                     To support, please contribute online via Venmo directly to steven.rosenblum4d211@gmail.com.
+                </p>
+                <p>
                     Thank you!
                 </p>
+                <div>
+                <img src="./images/QR Code.jpg" width="10%" alt="QR Code" />
+            </div>
             </div>
         </span>
 
